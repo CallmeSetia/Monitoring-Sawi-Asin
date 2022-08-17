1dc99913f44f538980e52de939743399390265f2
Dwi Setia Fardhana <84776462+CallmeSetia@users.noreply.github.com>
2022-08-17 16:30:58 +0700

    Koneksi Database

--- App/connection.php
+++ App/connection.php
@@ -1,8 +1,9 @@
 <?php
+    // KONEKSI DB
     $DB_HOST = "127.0.0.1";
     $DB_PORT = "80";
     $DB_USER = "root";
     $DB_PASS = "";
-    $DB_DATABASE = "project_monitoring-system";
+    $DB_DATABASE = "project_monitoring-system"; 
     $DB_CHARSET = "utf8";
 ?>
